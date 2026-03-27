91b4e0689c09a37657cc433aff7cd8f0a805a9ea
GALLA PRUDHVI RAJ <pruney1627@gmail.com>
2026-03-27 23:03:40 +0530

    learned about different string methods in java

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ else{
     console.log(`${day} is not a day`)
 }*/
 
-let day = 10;
+/*let day = 10;
 switch(day){
     case 1:
         console.log("It is Monday");
@@ -75,4 +75,39 @@ switch(day){
         break;
     default:
         console.log(`${day} is not a day`)
-}
+}*/
+
+
+
+
+
+//DAY - 3
+
+//string methods = allow you to manipulate and work with text(strings)
+
+
+/*let userName = "PRUDHVI RAJ";
+let print = userName.charAt(0)
+console.log(print);
+
+let print1 = userName.indexOf("D")
+console.log(print1);
+
+let print2 = userName.lastIndexOf("D")
+console.log(print2);
+
+let print3 = userName.length
+console.log(print3); 
+
+let print4 = userName.trim()
+console.log(print4);
+
+let print5 = userName.toLowerCase()
+console.log(print5);
+
+let print6 = userName.repeat(3)
+console.log(print6);*/
+
+
+
+//DAY - 4
