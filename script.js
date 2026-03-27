8d2d7b9944d2d450c931e74b5d2330273f986681
GALLA PRUDHVI RAJ <pruney1627@gmail.com>
2026-03-27 22:35:37 +0530

    learned about ternary operator in js

---
+++ script.js
@@ -0,0 +1,14 @@
+//ternary operator = a shortcut to if{} and else{} statements helps to assign a variable based on a condition 
+//condition ? codeIfTrue : codeIfFalse;
+
+let age = 21;
+let message = age >= 18 ? "You are an adult" : "You are a minor ";
+console.log(message);
+
+let time  = 9;
+let greeting = time < 12 ? "Good Morning!" : "Good Afternoon!";
+console.log(greeting);
+
+let isStudent = true;
+let message1 = isStudent? "You are a student" : "You are not a student";
+console.log(message1);
